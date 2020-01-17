a4511947f95e2cad560663c69f79f1cfa6974785
popcor255 <popcor255@gmail.com>
2020-01-17 16:54:51 -0500

    update

--- index.js
+++ index.js
@@ -2,33 +2,37 @@ var request = require('request');
 var { repos } = require('./repos.json');
 var date =  null;
 var last_date = null;
+
 require('dotenv').config();
 
-repos.forEach(repo => {
-    var options = {
-        headers : {
-         'User-Agent': 'Nodejs-App',
-         'Authorization': 'Bearer ' + process.env.API_TOKEN,
-         'Content-Type' : 'application/json; charset=utf-8'
-        },
-       'method': 'GET',
-       'url': repo + "?page=1&per_page=1"
-     };
-     
-     request(options, function (error, response) { 
-       if (error) throw new Error(error);
-       if (typeof response.body.message  == "string"){
-         return new Error(error);
-       }
-    
-       if(last_date !== date){
-            console.log("test");
-       }
+setInterval(getRepos, 5000);
 
-       last_date = date;
-       date = JSON.parse(response.body)[0].commit.committer.date;
-       
-     });
-});
+function getRepos(){
+    repos.forEach(repo => {
+        var options = {
+            headers : {
+            'User-Agent': 'Nodejs-App',
+            'Authorization': 'Bearer ' + process.env.API_TOKEN,
+            'Content-Type' : 'application/json; charset=utf-8'
+            },
+        'method': 'GET',
+        'url': repo + "?page=1&per_page=1"
+        };
+        
+        request(options, function (error, response) { 
+        if (error) throw new Error(error);
+        if (typeof response.body.message  == "string"){
+            return new Error(error);
+        }
+        
+        if(last_date !== date){
+                console.log("test");
+        }
 
+        last_date = date;
+        date = JSON.parse(response.body)[0].commit.committer.date;
+        
+        });
+    });
+}
 
