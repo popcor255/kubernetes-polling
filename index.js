3075b84a8d3fa0fd06e3ed244c7812b2e651c5c5
Napoleon Santana <napoleonsantana@Napoleons-MacBook-Pro.local>
2020-01-19 14:42:43 -0500

    update

--- index.js
+++ index.js
@@ -12,26 +12,27 @@ function getRepos(){
     var token = process.env.API_TOKEN;
     var uuid = random(Math.pow(10, 12), Math.pow(10, 13));
     
-  
     repos.forEach(repo => {
 
         var { gitRequest, pipelineRunRequest, pipelineRerunRequest } = getRequests(repo, uuid);
 
-        
         request(gitRequest, function (error, response) {
-            validate(token, error, response); 
+            validate(error, response, process.env); 
 
             let new_date = getLastCommitter(response);
 
             if(new_date !== date){    
                 if(isFirstRequest(date)){
                     request(pipelineRunRequest, function (error, response) { 
-                        validate(token, error, response);
+                        validate(error, response);
                         console.log(response);
                     });
                 }
                 else{
-                    
+                    request(pipelineRerunRequest, function (error, response) { 
+                        validate(error, response);
+                        console.log(response);
+                    });
                 }
 
                 date = new_date;
@@ -47,8 +48,13 @@ function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function validate(token, error, response){
-    if(!token) throw new Error("API TOKEN NOT FOUND AS ENV");
+function validate(error, response, env){
+    if(env == null){
+        console.log("WARNING: NO ENV VARIABLES WAS PASSED, CANT VALIDATE ENV");
+    }
+    else{
+        if(!env.API_TOKEN) throw new Error("API TOKEN NOT FOUND AS ENV");
+    }
     if (error) throw new Error(error);
     if (typeof response.body.message  == "string" ){
         return new Error(error);
@@ -63,7 +69,6 @@ function getLastCommitter(response){
     return JSON.parse(response.body)[0].commit.committer.date;
 }
 
-
 function getRequests(repo, uuid){
     var gitRequest = {
         headers : {
@@ -77,7 +82,7 @@ function getRequests(repo, uuid){
 
     var pipelineRunRequest = {
         'method': 'POST',
-        'url': 'http://localhost:9097/proxy/apis/tekton.dev/v1alpha1/namespaces/default/pipelineruns/',
+        'url': 'http://' + process.env.IP + ':' + process.env.PORT + '/proxy/apis/tekton.dev/v1alpha1/namespaces/default/pipelineruns/',
         'headers': {
           'Content-Type': 'application/json'
         },
@@ -85,7 +90,12 @@ function getRequests(repo, uuid){
     };
 
     var pipelineRerunRequest = {
-
+        'method': 'POST',
+        'url': 'http://' + process.env.IP + ':' + process.env.PORT + '/namespaces/default/rerun/',
+        'headers': {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({"pipelinerunname":"mypipeline-run-" + uuid})
     };
 
     return { gitRequest, pipelineRunRequest, pipelineRerunRequest };
