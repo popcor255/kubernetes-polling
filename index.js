57b2e6af899a7e28d6e025e1a781c6ac5e4827d1
Napoleon Santana <napoleonsantana@Napoleons-MacBook-Pro.local>
2020-01-18 21:27:17 -0500

    update

--- index.js
+++ index.js
@@ -15,8 +15,17 @@ function getRepos(){
             'Authorization': 'Bearer ' + process.env.API_TOKEN,
             'Content-Type' : 'application/json; charset=utf-8'
             },
-        'method': 'GET',
-        'url': repo + "?page=1&per_page=1"
+            'method': 'GET',
+            'url': repo + "?page=1&per_page=1"
+        };
+
+        var tektonRequest = {
+            'method': 'POST',
+            'url': 'http://localhost:9097/proxy/apis/tekton.dev/v1alpha1/namespaces/tekton-pipelines/pipelineruns/',
+            'headers': {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({"apiVersion":"tekton.dev/v1alpha1","kind":"PipelineRun","metadata":{"name":"mypipeline-run-1579400761634","labels":{"tekton.dev/pipeline":"mypipeline","app":"tekton-app"}},"spec":{"pipelineRef":{"name":"mypipeline"},"resources":[],"params":[],"timeout":"60m"}})  
         };
         
         request(options, function (error, response) { 
@@ -27,11 +36,19 @@ function getRepos(){
 
             let new_date = JSON.parse(response.body)[0].commit.committer.date;
 
-            if(new_date !== date){    
-                console.log("test");
+            if(new_date !== date && date != null){    
+                request(tektonRequest, function (error, response) { 
+                    if (error) throw new Error(error);
+                    console.log(response.body);
+                });
                 date = new_date;
             }
+
         });
+
+          
+
+
     });
 }
 
